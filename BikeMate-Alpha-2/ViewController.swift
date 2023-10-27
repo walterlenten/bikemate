f0e0b73463ef0df9682dd8f4cb8083c6363cf29d
Asher Walter <walterlenten@gmail.com>
2023-10-27 10:41:50 +1000

    First visual demo build of BikeMate.
    No functions added, just a visual demo.
    
    Co-Authored-By: Brock <147293696+brock405@users.noreply.github.com>

---
+++ BikeMate-Alpha-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BikeMate-Alpha-2
+//
+//  Created by Administrator on 25/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
